04359832cd1e309416224d595c63f0087c813cff
JAPPS102 <chubukin9898@gmail.com>
2024-01-18 17:41:16 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import moment from 'moment';
+import 'moment/locale/ru';
 import './App.css';
 
 const ContributionGraph = () => {
   const today = new Date();
-  const startDate = new Date(today);
-  startDate.setDate(startDate.getDate() - 357);
+  const startDate = moment(today).subtract(50, 'weeks').toDate();
   const [contributionsData, setContributionsData] = useState(null);
+  const [selectedCell, setSelectedCell] = useState(null);
+  const contributionGraphRef = useRef(null);
+
 
   const weekdays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
 
   const months = Array.from({ length: 12 }, (_, monthIndex) => {
-    const monthStartDate = new Date(startDate);
-    monthStartDate.setMonth(monthStartDate.getMonth() + monthIndex);
+    const monthStartDate = moment(startDate).add(monthIndex, 'months').toDate();
 
     return {
       monthName: monthStartDate.toLocaleString('default', { month: 'short' }),
       weeks: Array.from({ length: 4 }, (_, weekIndex) => {
-        const weekStartDate = new Date(monthStartDate);
-        weekStartDate.setDate(weekStartDate.getDate() + weekIndex * 7);
+        const weekStartDate = moment(monthStartDate).startOf('month').add(weekIndex, 'weeks').startOf('isoWeek').toDate();
 
         return {
           weekNumber: weekIndex + 1,
           days: Array.from({ length: 7 }, (_, dayIndex) => {
-            const currentDate = new Date(weekStartDate);
-            currentDate.setDate(currentDate.getDate() + dayIndex);
+            const currentDate = moment(weekStartDate).add(dayIndex, 'days').toDate();
             return {
               dayNumber: currentDate.getDate(),
               date: currentDate.toISOString().slice(0, 10),
@@ -33,6 +34,7 @@ const ContributionGraph = () => {
       }),
     };
   });
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,44 +58,100 @@ const ContributionGraph = () => {
     else return 'very-high-contributions';
   };
 
+  const formatTooltipDate = (date) => {
+
+    const formattedDate = moment(date).locale('ru').format('dddd, MMMM D, YYYY');
+
+    return formattedDate;
+  };
+
+
+  const handleCellClick = (contributions, date) => {
+    setSelectedCell({ contributions, date });
+  };
+
+  const handleOutsideClick = (event) => {
+    if (contributionGraphRef.current && !contributionGraphRef.current.contains(event.target)) {
+      setSelectedCell(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
+
 
   return (
-    <div className="contribution-wrapper">
-      <div className="contribution-graph">
-        <div className="month">
-          <div className="weeks">
-            <div className="days weekdays">
-              {weekdays.map((weekday, index) => (
-                <div key={index} className="weekday">
-                  {weekday}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        {months.map((month, index) => (
-          <div key={index} className="month">
-            <div className="month-name">{month.monthName}</div>
+    <div>
+      <div className="contribution-wrapper" ref={contributionGraphRef}>
+        <div className="contribution-graph">
+          <div className="month">
             <div className="weeks">
-              {month.weeks.map((week, weekIndex) => (
-                <div key={weekIndex} className="week">
-                  <div className="week-number"></div>
-                  <div className="days">
-                    {week.days.map((day) => (
-                      <div key={day.dayNumber} className={`day ${day.date === today.toISOString().slice(0, 10) ? 'current-day' : ''}`} data-date={day.date}>
-                        <div className={`contribution-cell ${getColorClass(contributionsData?.[day.date] || 0)}`}></div>
-                        { }
-                      </div>
-                    ))}
+              <div className="days weekdays">
+                {weekdays.map((weekday, index) => (
+                  <div key={index} className="weekday">
+                    {weekday}
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        ))}
+          {months.map((month, index) => (
+            <div key={index} className="month">
+              <div className="month-name">{month.monthName}</div>
+              <div className="weeks">
+                {month.weeks.map((week, weekIndex) => (
+                  <div key={weekIndex} className="week">
+                    <div className="week-number"></div>
+                    <div className="days">
+                      {[1, 2, 3, 4, 5, 6, 0].map((dayIndex) => {
+                        const day = week.days[dayIndex];
+                        return (
+                          <div
+                            key={day.dayNumber}
+                            className={`day ${day.date === today.toISOString().slice(0, 10) ? 'current-day' : ''} ${selectedCell && selectedCell.date === day.date ? 'selected-cell' : ''}`}
+                            data-date={day.date}
+                            onClick={() => handleCellClick(contributionsData?.[day.date] || 0, day.date)}
+                          >
+                            <div className={`contribution-cell ${getColorClass(contributionsData?.[day.date] || 0)}`}>
+                              {selectedCell && selectedCell.date === day.date && (
+                                <div className="tooltip">
+                                  <div className="countContribution">{selectedCell.contributions} contributions</div>
+                                  <div className="dateContribution">{formatTooltipDate(selectedCell.date)}</div>
+                                  <div className="arrowTooltip"></div>
+                                </div>
+                              )}
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="contribution-graph-clue">
+        <div className="title">Меньше</div>
+        <div className="contribution-cell">
+        </div>
+        <div className="contribution-cell medium-contributions">
+        </div>
+        <div className="contribution-cell high-contributions">
+        </div>
+        <div className="contribution-cell very-high-contributions">
+        </div>
+        <div className="title">Больше</div>
       </div>
     </div>
-
   );
 };
+
 export default ContributionGraph;
